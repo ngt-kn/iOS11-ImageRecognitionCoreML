5a433d3947e2fd0db0c7755275398eef4059e233
ngt-kn <nagatak@dev.local>
2018-05-15 01:06:41 -0700

    initial commit

--- ImageRecognitionCoreML/ViewController.swift
+++ ImageRecognitionCoreML/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var cameraImageView: UIImageView!
+    
+    // Create new imagePicker object
+    let imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setup the imagepicker
+        imagePicker.delegate = self
+        // change sourceType = .photoLibrary to allow user to use thier photos instead of camera
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        
+    }
+    
+    // imagePicker delegate method
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // Get the image user has selected
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            cameraImageView.image = selectedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
     }
-
-
+    
 }
 
