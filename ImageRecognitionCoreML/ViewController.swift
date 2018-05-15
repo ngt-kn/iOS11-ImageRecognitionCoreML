618f1d3e3761ade9a65ea2808de4e7848df0d21f
ngt-kn <nagatak@dev.local>
2018-05-15 01:31:49 -0700

    adds inceptionv3 image recognition

--- ImageRecognitionCoreML/ViewController.swift
+++ ImageRecognitionCoreML/ViewController.swift
@@ -36,10 +36,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Get the image user has selected
         if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             cameraImageView.image = selectedImage
+            
+            guard let ciimage = CIImage(image: selectedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+        
+            detect(image: ciimage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
